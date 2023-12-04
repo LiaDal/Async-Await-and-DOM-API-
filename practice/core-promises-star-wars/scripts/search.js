3791d81b085e4192fbe82af02c3126ebc33ae605
julia <julia18g11@gmail.com>
2023-12-04 15:29:38 +0300

    complete first task

--- practice/core-promises-star-wars/scripts/search.js
+++ practice/core-promises-star-wars/scripts/search.js
@@ -7,3 +7,48 @@
 // starWars.getSpeciesById(id)
 
 // Тут ваш код.
+
+let searchBtn = document.getElementById("byQueryBtn");
+let contentField = document.getElementById("content");
+let searchInput = document.getElementById("searchInput");
+let resultBlock = document.getElementById("result-container")
+let loader = document.getElementById("spinner")
+
+
+  searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const searchText = searchInput.value;
+  loader.style.visibility = 'visible';
+
+  if (searchText.trim() === '') {
+    contentField.innerHTML = '';
+      return;
+  }
+
+  fetch(`https://swapi.dev/api/people/?search=${searchText}`)
+      .then((response) => response.json())
+      .then((results) => {
+        loader.style.visibility = 'hidden';
+        resultBlock.style.visibility = 'visible';
+        contentField.innerHTML = results.results.map((result) => `
+              Name: ${result.name},<br>
+              Height: ${result.height},<br>
+              Mass: ${result.mass},<br>
+              Haircolor: ${result.hair_color},<br>
+              Skincolor: ${result.skin_color},<br>
+              Eyecolor: ${result.eye_color},<br>
+              Birthyear: ${result.birth_year},<br>
+              Gender: ${result.gender},<br>
+              Homeworld: ${result.homeworld},<br>
+              Films: ${result.films.join("<br>")},<br>
+              Species: ${result.species},<br>
+              Vehicles: ${result.vehicles},<br>
+              Starships: ${result.starships},<br>
+              Created: ${result.created},<br>
+              edited: ${result.edited},<br>
+              url: ${result.url},<br>
+              `);
+      });
+});
+
+
